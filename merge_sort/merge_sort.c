e5f90342ac5977014561220f17d41bf089425612
Ben Muthalaly <benmuthalaly@gmail.com>
2023-10-13 04:06:44 -0500

    Switch from arrays to lists
    Can't figure out how to create a linked list!

--- merge_sort/merge_sort.c
+++ merge_sort/merge_sort.c
@@ -1,50 +1,68 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int NUM_ELEMENTS = 10;
 
-// example recursive function
-int fib(size_t n) {
-    if (n == 1) {
-        return 0;
-    }
-
-    if (n == 2) {
-        return 1;
-    }
-    return fib(n - 1) + fib(n - 2);
-}
+struct Node {
+    char val;
+    struct Node *next;
+};
 
 // create an array num_elements long filled with random integers
-char *create_list(int num_elements) {
+struct Node *create_list(int num_elements) {
+    struct Node *tmp = (struct Node *)malloc(sizeof(struct Node));
+    tmp->val = rand();
+    struct Node *head = tmp;
+    for (int i = 0; i < num_elements; i++) {
+        struct Node *next = (struct Node *)malloc(sizeof(struct Node));
+        next->val = rand();
+        printf("new node val: %d\n", next->val);
+        printf("new node address: %p\n", &next);
+        tmp->next = next;
+    }
+
+    return head;
+}
+
+void iterate_through_list(struct Node *head) {
+    printf("ITERATING\n");
+    int i = 0;
+    while (head->next) {
+        printf("node value: %d\n", head->val);
+        printf("next address %p\n", head->next);
+        head = head->next;
+        i++;
+    }
+    printf("list is %d elements long\n", i);
 }
 
 int main(void) {
-    char *rand_nums = create_list(NUM_ELEMENTS);
-    for (size_t i = 0; i < NUM_ELEMENTS; i++) {
-        printf("%d\n", rand_nums[i]);
-    }
+    // use time as seed for random list node values
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC);
+    srand(ts.tv_sec);
+
+    // struct list_node tmp = {.val = rand()};
+    // struct list_node tmp2 = {.val = rand(), .next = &tmp};
+
+    // printf("%d\n", tmp2.next == NULL);
+
+    struct Node *head = create_list(NUM_ELEMENTS);
+    // iterate_through_list(head);
+    printf("first element value: %d\n", head->val);
     return 0;
 }
 
-char *sort_array(char array) {
-    int len = sizeof(array) / sizeof(array[0]);
-    int mid = len / 2 char *left = malloc(mid);
+/*
+    Merge Sort overview
+    given a list
 
-    return merge(left, right);
-}
-
-char *merge(array1, array2)
-
-    /*
-        Merge Sort overview
-        given a list
-
-        sort_list:
-            check if a list is empty or 1 element long
-                if so, it's sorted, return it
-            split list down the middle into left and right sides
-            call sort_list on left and right sides
-            merge the two sorted lists
-    */
+    sort_list:
+        check if a list is empty or 1 element long
+            if so, it's sorted, return it
+        split list down the middle into left and right sides
+        call sort_list on left and right sides
+        merge the two sorted lists
+*/
