7181609291cc96739d1cd7a8e05bb9fc407b4452
Ben Muthalaly <benmuthalaly@gmail.com>
2023-10-16 13:03:51 -0500

    Fix partitioning

--- quick_sort/quick_sort.c
+++ quick_sort/quick_sort.c
@@ -2,69 +2,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Nice_Array {
-    int len;
-    int list[10];
-};
+void swap(int *a, int idx_1, int idx_2) {
+    int tmp = a[idx_1];
+    a[idx_1] = a[idx_2];
+    a[idx_2] = tmp;
+}
 
-void sort_arr(struct Nice_Array arr) {
-    if (arr.len < 2) {
+// p points to the first element, r points to the last element (which is the
+// pivot)
+int partition(int *a, int p, int r) {
+    // q eventually points to the pivot
+    // until the pivot is swapped into place, q points to the
+    // first element in the array found to be greater than the pivot value.
+    // if we find a new element that's greater than the pivot value, it's
+    // swapped with a[q] and we increment q so that all elements found to be
+    // greater than the pivot value remain to the right of q
+    int q = p;
+    // j is our iterating variable
+    int j = p;
+
+    // look at every element besides the pivot
+    while (j < r) {
+        if (a[j] > a[r]) {
+            j++;
+        }
+        if (a[j] < a[r]) {
+            swap(a, j, q);
+            j++;
+            q++;
+        }
+    }
+
+    // swap the pivot value (a[r]) with the element in between the Greater and
+    // Less groups a[q]
+    swap(a, r, q);
+
+    return q;
+}
+
+// sub array range is [p, r). p and r are both indices
+void sort_arr(int *a, int p, int r) {
+    if ((r - p + 1) < 2) {
         return;
     }
 
-    // select the last element to always be the partition index
-    int part_idx = arr.len - 1;
+    int part_idx = partition(a, p, r);
+
+    for (int i = 0; i <= r; i++) {
+        printf("i: %d \t val: %d\n", i, a[i]);
+    }
+
     printf("partition index: %d\n", part_idx);
-    printf("partition index value: %d\n", arr.list[part_idx]);
-
-    int second_ptr = 0;
-
-    for (int i = 0; i < arr.len; i++) {
-        printf("%d\n", i);
-        // find an element greater than the partition element and remember it
-        if (arr.list[i] > arr.list[part_idx]) {
-            second_ptr = i;
-        } else {
-            continue;
-        }
-        // find an element less than the partition element and swap it with the
-        // greater element
-        if (arr.list[i] < arr.list[part_idx]) {
-            int tmp = arr.list[i];
-            arr.list[i] = arr.list[second_ptr];
-            arr.list[second_ptr] = tmp;
-            i = second_ptr + 1;
-        }
-    }
-
-    // swap partition index with last greater element found
-    int tmp = arr.list[part_idx];
-    arr.list[part_idx] = arr.list[second_ptr];
-    arr.list[second_ptr] = tmp;
-
-    for (int i = 0; i < arr.len; i++) {
-        // if (i == mid_idx) {
-        //     printf("at the middle %d\n", i);
-        // }
-        printf("index %d val: %d\n", i, arr.list[i]);
-    }
-    printf("partition index value: %d\n", arr.list[part_idx]);
 
     return;
 }
 
 int main(void) {
-    struct Nice_Array arr = {.len = 10, .list = {3, 4, 2, 7, 0, 1, 9, 7, 8, 5}};
-
-    // int tmp = arr.list[0];
-    // arr.list[0] = arr.list[6];
-    // arr.list[6] = tmp;
-
-    // for (int i = 0; i < arr.len; i++) {
-    //     // if (i == mid_idx) {
-    //     //     printf("at the middle %d\n", i);
-    //     // }
-    //     printf("index %d val: %d\n", i, arr.list[i]);
+    int a[] = {3, 4, 2, 7, 0, 1, 9, 7, 8, 5};
+    int r = 9;
+    // for (int i = 0; i <= r; i++) {
+    //     printf("i: %d \t val: %d\n", i, a[i]);
     // }
-    sort_arr(arr);
+    sort_arr(a, 0, r);
 }
